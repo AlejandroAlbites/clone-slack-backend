38eec25e43f31ef892e1f098a828b87d94d17bf3
AlejandroAlbites <81944337+AlejandroAlbites@users.noreply.github.com>
2022-05-30 08:53:52 -0500

    feat: update channel collection

--- src/routes/channel.js
+++ src/routes/channel.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const channelController = require('../controllers/channel.controller');
 
 router.route('/').get(channelController.list);
+router.route('/:channelId').get(channelController.show);
 router.route('/').post(channelController.create);
+router.route('/:channelId').put(channelController.update);
+router.route('/:channelId').delete(channelController.destroy);
 
 module.exports = router;

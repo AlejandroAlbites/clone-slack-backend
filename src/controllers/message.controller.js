5f7c7805349b782870328eecae1c0dc6ca551412
Edinael <edsanol99@gmail.com>
2022-06-10 13:33:35 -0500

    chat direct

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,24 @@
+const Message = require('../models/message.model');
+
+const getChatMessage = async (req, res) => {
+  const miId = req.uid;
+  const mensajesTo = req.params.de;
+
+  const last30 = await Message.find({
+    $or: [
+      { from: miId, to: mensajesTo },
+      { from: mensajesTo, to: miId },
+    ],
+  })
+    .sort({ createdAt: 'asc' })
+    .limit(30);
+
+  res.json({
+    ok: true,
+    msg: last30,
+  });
+};
+
+module.exports = {
+  getChatMessage,
+};
